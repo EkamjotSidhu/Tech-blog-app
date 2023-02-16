73f4d3e2718958441e346db3ae64c36cb9fed7b6
EkamjotSidhu <ekamsidhu5678@gmail.com>
2023-02-16 00:01:22 -0500

    Updated repo

--- utils/auth.js
+++
@@ -1,9 +0,0 @@
-const withAuth = (req, res, next) => {
-    if (!req.session.userName_id) {
-      res.redirect('/login');
-    } else {
-      next();
-    }
-  };
-  
-  module.exports = withAuth;
